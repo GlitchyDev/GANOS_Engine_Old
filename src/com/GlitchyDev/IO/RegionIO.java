38b68b87d6b2f21058e8a201e9c8cffe695b8886
GlitchyDev <RobertLouisHannah@gmail.com>
2019-02-04 09:03:31 -0500

    Update #56 Rewrite ChunkIO, Entities, Block
    Optional Old File Type

--- src/com/GlitchyDev/IO/RegionIO.java
+++ src/com/GlitchyDev/IO/RegionIO.java
@@ -7,7 +7,7 @@ import com.GlitchyDev.World.Entities.EntityType;
 import com.GlitchyDev.World.Location;
 import com.GlitchyDev.World.Region;
 import com.GlitchyDev.World.Utility.HuffmanTreeUtility;
-import com.sun.deploy.util.ArrayUtil;
+import com.GlitchyDev.World.Utility.RegionFileType;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -29,94 +29,100 @@ public class RegionIO {
         ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
 
         // Use to confirm compatibility
-        int versionNum = getCorrectedByteValue(objectInputStream.readByte());
+        int versionNum = getCorrectedIntValue(objectInputStream.readByte());
         if(!(versionNum <= CURRENT_VERSION && versionNum >= LEAST_SUPPORTED_VERSION)) {
             System.out.println("RegionIO: Error, Version " + versionNum + " not supported");
         }
-        /*
+
+
         // Use to get fileType, like XL palette, ect. Don't support until needed
-        byte fileType = objectInputStream.readByte();
-        */
+        RegionFileType regionFileType = RegionFileType.values()[getCorrectedIntValue(objectInputStream.readByte())];
 
-        int width = objectInputStream.readByte();
-        int length = objectInputStream.readByte();
-        int height = objectInputStream.readByte();
+        if(regionFileType == RegionFileType.NORMAL) {
+            int width = getCorrectedIntValue(objectInputStream.readByte());
+            int length = getCorrectedIntValue(objectInputStream.readByte());
+            int height = getCorrectedIntValue(objectInputStream.readByte());
 
-        int blockPaletteSize = getCorrectedByteValue(objectInputStream.readByte());
-        int entityPaletteSize = getCorrectedByteValue(objectInputStream.readByte());
-
-
-        Object[] sharedPallete = new Object[blockPaletteSize + entityPaletteSize];
-        int[] sharedFrequency = new int[blockPaletteSize + entityPaletteSize];
-
-        for(int i = 0; i < blockPaletteSize; i++) {
-            // Check Blocks
-            BlockType blockType = BlockType.values()[getCorrectedByteValue(objectInputStream.readByte())];
-            BlockBase block = blockType.getBlockClass();
-            block.readData(objectInputStream);
-            sharedPallete[i] = block;
-            sharedFrequency[i] = getCorrectedByteValue(objectInputStream.readByte());
-        }
-
-        int entityCount = 0;
-        for(int i = blockPaletteSize; i < blockPaletteSize + entityPaletteSize; i++) {
-            // Add Entities
-            EntityType entityType = EntityType.values()[getCorrectedByteValue(objectInputStream.readByte())];
-            EntityBase entity = entityType.getEntityClass();
-            entity.readData(objectInputStream);
-            sharedPallete[i] = entity;
-            sharedFrequency[i] = getCorrectedByteValue(objectInputStream.readByte());
-            entityCount += sharedFrequency[i];
-
-        }
+            int blockPaletteSize = getCorrectedIntValue(objectInputStream.readByte());
+            int entityPaletteSize = getCorrectedIntValue(objectInputStream.readByte());
 
 
-        HashMap<String,Object> encodeMap = HuffmanTreeUtility.getHuffmanValues(sharedPallete,sharedFrequency);
-        BlockBase[][][] blocks = new BlockBase[height][width][length];
-        ArrayList<EntityBase> entities = new ArrayList<>(entityCount);
+            Object[] sharedPallete = new Object[blockPaletteSize + entityPaletteSize];
+            int[] sharedFrequency = new int[blockPaletteSize + entityPaletteSize];
 
-        byte currentByte = 0;
-        int pos = 8;
-        int blockCount = 0;
-
-        int x = 0;
-        int y = 0;
-        int z = 0;
-        for(int entryCount = 0; entryCount < (width * length * height) + entityCount; entityCount++) {
-            String currentCode = "";
-
-
-            while(!encodeMap.containsKey(currentCode)) {
-                currentCode += getByteValue(currentByte,pos);
-                pos++;
-                if(pos == 8) {
-                    currentByte = objectInputStream.readByte();
-                }
+            for (int i = 0; i < blockPaletteSize; i++) {
+                // Check Blocks
+                BlockType blockType = BlockType.values()[getCorrectedIntValue(objectInputStream.readByte())];
+                BlockBase block = blockType.getBlockClass();
+                block.readData(objectInputStream);
+                sharedPallete[i] = block;
+                sharedFrequency[i] = getCorrectedIntValue(objectInputStream.readByte());
             }
-            Object readObject = encodeMap.get(currentCode);
 
-            if(readObject instanceof BlockBase) {
-                // REQUIRE CLONE
-                BlockBase readBlock = ((BlockBase) readObject).clone();
+            int entityCount = 0;
+            for (int i = blockPaletteSize; i < blockPaletteSize + entityPaletteSize; i++) {
+                // Add Entities
+                EntityType entityType = EntityType.values()[getCorrectedIntValue(objectInputStream.readByte())];
+                EntityBase entity = entityType.getEntityClass();
+                entity.readData(objectInputStream);
+                sharedPallete[i] = entity;
+                sharedFrequency[i] = getCorrectedIntValue(objectInputStream.readByte());
+                entityCount += sharedFrequency[i];
 
-                y = blockCount / (width * length);
-                x = blockCount % width;
-                z = blockCount / width;
-                blocks[y][x][z] = readBlock;
+            }
 
-                blockCount++;
-            } else {
-                if(readObject instanceof EntityBase) {
+
+            HashMap<String, Object> encodeMap = HuffmanTreeUtility.getHuffmanValues(sharedPallete, sharedFrequency);
+            BlockBase[][][] blocks = new BlockBase[height][width][length];
+            ArrayList<EntityBase> entities = new ArrayList<>(entityCount);
+
+            byte currentByte = 0;
+            int pos = 8;
+            int blockCount = 0;
+
+            int x = 0;
+            int y = 0;
+            int z = 0;
+            for (int entryCount = 0; entryCount < (width * length * height) + entityCount; entityCount++) {
+                String currentCode = "";
+
+
+                while (!encodeMap.containsKey(currentCode)) {
+                    currentCode += getByteValue(currentByte, pos);
+                    pos++;
+                    if (pos == 8) {
+                        currentByte = objectInputStream.readByte();
+                    }
+                }
+                Object readObject = encodeMap.get(currentCode);
+
+                if (readObject instanceof BlockBase) {
                     // REQUIRE CLONE
-                    EntityBase readEntity = ((EntityBase) readObject).clone();
-                    readEntity.setLocation(new Location(x,y,z,null));
-                    // Set Entity Position from x y z from last read block
-                    entities.add(readEntity);
-                }
-            }
-            // Grab object read
-        }
+                    BlockBase readBlock = ((BlockBase) readObject).clone();
 
+                    y = blockCount / (width * length);
+                    x = blockCount % width;
+                    z = blockCount / width;
+                    blocks[y][x][z] = readBlock;
+
+                    blockCount++;
+                } else {
+                    if (readObject instanceof EntityBase) {
+                        // REQUIRE CLONE
+                        EntityBase readEntity = ((EntityBase) readObject).clone();
+                        readEntity.setLocation(new Location(x, y, z, null));
+                        // Set Entity Position from x y z from last read block
+                        entities.add(readEntity);
+                    }
+                }
+                // Grab object read
+            }
+        } else {
+
+
+
+            
+        }
 
 
 
@@ -149,7 +155,12 @@ public class RegionIO {
         return ((b >> (pos - 1)) & 1) == 1;
     }
 
-    private static int getCorrectedByteValue(byte b) {
+    private static int getCorrectedIntValue(byte b) {
         return (b>=0) ? b : (127 + (129 + b));
+    }
+
+    private static byte getCorrectByteValue(int i) {
+        return (byte) (-128 + (i));
+
     }
 }
